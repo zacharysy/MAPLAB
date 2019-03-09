a49b8e7c80f1bb51caeb8b6ccf9651ace44507ec
Zachary Sy <zsy@nd.edu>
2019-03-08 21:54:21 -0500

    Update Node.m
    bug fixes and performance enhancements

--- Node.m
+++ Node.m
@@ -3,13 +3,12 @@ classdef Node
         Name % String
         AdjacentNodes % Array of Nodes
         AdjacentDistances % $Array of Ints (can't put different data types in array so gotta have two arrays :( )
-        Coord % Array of Array of Ints
         Building % Boolean
     end
 
     methods
         % Initializes the class
-        function obj = Node(name,coord,building)
+        function obj = Node(name,building)
             if nargin < 3
                 obj.Building = 0;
             else
@@ -17,7 +16,6 @@ classdef Node
             end
 
             obj.Name = name;
-            obj.Coord = coord;
             obj.AdjacentNodes = [];
             obj.AdjacentDistances = [];
         end
