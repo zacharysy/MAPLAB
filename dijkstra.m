765311f467dfd2c1430f39ba25a846fbf29b6909
Zachary Sy <zsy@nd.edu>
2019-03-10 19:45:35 -0400

    wrote comments so u know wtf I did

--- dijkstra.m
+++ dijkstra.m
@@ -22,7 +22,7 @@ function [path,finalDist] = dijkstra(nodes)
         nodeDist = currentNode.AdjacentDistances;
         adjacents = currentNode.AdjacentNodes;
 
-        % Travel to adjacent nodes
+        % Records distances to adjacent nodes
         for i = 1:length(adjacents)
             [~,nodeIndex] = nodeInArray(nodes,currentNode.Name);
             [~,j] = nodeInArray(nodes,adjacents(i));
@@ -34,16 +34,21 @@ function [path,finalDist] = dijkstra(nodes)
         end
 
         visited = [visited,currentNode];
+
+%% Finding Next Node with Least Distance
         count = 0;
         i = least(distances,count);
         [bool,~] = nodeInArray(visited,nodes(i).Name);
-        
+
+        % repeats until an univisited node with least distance is found
         while bool
             count = count + 1;
             i = least(distances,count);
             [bool,~] = nodeInArray(visited,nodes(i).Name);
         end
-        
+
+%% Update current node! yay.
+
         fromNode(i) = currentNode.Name;
         currentNode = nodes(i);
         
@@ -56,11 +61,15 @@ function [path,finalDist] = dijkstra(nodes)
 end
 
 
+%% Some functions I had to make cuz MATLAB doesn't have shit I need
+
+% Gets index of node with least distance
 function i = least(nodeDist,ignore)
     sorted = sort(nodeDist);
     i = find(nodeDist==sorted(1+ignore),1);
 end
 
+% Finds if node with name 'node' is in an array
 function [bool,index] = nodeInArray(arr,node)
     bool = 0;
     index = 0;
