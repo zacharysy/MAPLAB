84567906711e0516d6b38f9c78909eb08049d891
Zachary Sy <zsy@nd.edu>
2019-03-20 23:45:14 -0400

    summary

--- test.m
+++ test.m
@@ -7,12 +7,12 @@ weights = [10 5 2 1 5];
 nodeNames = {'N1' 'N2' 'N3' 'N4'};
 G = graph(s,t,weights,nodeNames);
 
-%plot(G,'EdgeLabel',G.Edges.Weight)
+plot(G,'EdgeLabel',G.Edges.Weight)
 %[a, b] = nns(nodes, node2);
 %disp(a);
 %disp(b);
 
-[path,dist] = dijkstra(G,'N1');
+[path,dist] = dijkstra(G,'N4','N1')
 [p, d] = nns(G, 'N1', 'N4')
 
 %
