075fa7d468f559835d69301cce350597d934dee8
Zachary Sy <zsy@nd.edu>
2019-04-06 16:16:17 -0400

    made longest path

--- test.m
+++ test.m
@@ -11,7 +11,7 @@ plot(G,'EdgeLabel',G.Edges.Weight)
 %[a, b] = nns(nodes, node2);
 %disp(a);
 %disp(b);
-[path,dist] = dijkstra(G,'N4','N1')
+[path,dist] = longestPath(G,'N1','N4')
 [p, d] = nns(G, 'N1', 'N4')
 
 %
