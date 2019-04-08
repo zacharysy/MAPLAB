53eb7e43f6bca09ba66273ee7bc6b5b0c99a3e81
Patrick Soga <psoga117@gmail.com>
2019-04-08 17:32:14 -0400

    adf

--- test.m
+++ test.m
@@ -4,8 +4,8 @@ clc
 s = [1 1 2 2 3 ];
 t = [2 3 3 4 4 ];
 weights = [10 5 2 1 5];
-nodeNames = {'N1' 'fuck' 'N3' 'lol'};
-buildings = {'fuck','lol'};
+nodeNames = {'N1' 'LaFun' 'N3' 'Hesburgh'};
+buildings = {'LaFun','Hesburgh'};
 %buildings = cell2table(buildings)
 G = graph(s,t,weights,nodeNames);
 
@@ -17,13 +17,13 @@ plot(G,'EdgeLabel',G.Edges.Weight)
 
 % [path,dist] = dijkstra(G,'N4','N1')
 
-[path,dist] = longestPath(G,'N1','N4')
-[p, d] = nns(G, 'N1', 'N4', 'N0')
+[path,dist] = longestPath(G,'N1','Hesburgh')
+[p, d] = nns(G, 'N1', 'Hesburgh')
 G = graphModifier(G,buildings,1/12);
 figure
 plot(G,'EdgeLabel',G.Edges.Weight)
-[path,dist] = dijkstra(G,'N1','lol');
-[p, d] = nns(G, 'N1', 'lol');
+[path,dist] = dijkstra(G,'N1','Hesburgh');
+[p, d] = nns(G, 'N1', 'Hesburgh');
 
 %
 %   --5--n3
