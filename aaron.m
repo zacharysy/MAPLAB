16f30c64016b9b5516f20c71625d9f1d5654b84e
Aaron Diaz <aarondiaz@aarons-mbp-3.dhcp.nd.edu>
2019-04-22 15:21:42 -0400

    map

--- aaron.m
+++ aaron.m
@@ -23,9 +23,9 @@ clc
 
 %% Make Graph
 
-buildings = {'rockneMem', 'lyons', 'morissey', 'howard', 'pangborn', 'fisher'}';
+buildings = {'Rockne Memorial', 'Lyons Hall', 'Morissey Hall', 'Howard Hall', 'Pangborn Hall', 'Fisher Hall', 'South Dining Hall', 'Badin Hall'}';
 intersections = {};
-for i=1:4
+for i=1:20
     intersections{i} = ['i', num2str(i)];
 end
 
@@ -33,13 +33,21 @@ nodes = cat(1, buildings, intersections');
 xVal = zeros(10,1);
 yVal = zeros(10,1);
 bump = length(buildings);
-s = [1 1 1 2 7];
-t = [bump+1 8 9 10 8];
-weights = [1000 200 33 40 33];
+s = [1 1 1 bump+7 bump+7 bump+7 bump+7 bump+7 bump+7 5 5 5 6 bump+3 bump+3 7 7 7 7 bump+4 8,...
+    bump+5 bump+5 bump+16 bump+17 bump+11 bump+11 bump+11 bump+11 bump+6 bump+6 bump+6 bump+6,...
+    bump+10 bump+10 bump+10 3 bump+12 bump+12 bump+14 bump+14 bump+14 bump+13 bump+9 bump+15];
+
+t = [bump+1 bump+2 bump+7 bump+14 bump+13 bump+8 bump+18 5 bump+1 bump+2 6 ,...
+    bump+18 bump+3 bump+20 7 bump+20 bump+4 8 bump+5 8 bump+5 bump+16 bump+17 4 4,...
+    4 bump+10 bump+9 bump+6 bump+9 bump+8 bump+18 bump+20 3 bump+9 bump+12 bump+12,...
+    bump+14 bump+13 bump+15 2 bump+13 bump+8 bump+8 2];
 
 
-nodeTable = table(nodes,xVal,yVal,'VariableNames',{'Name','XCoord','YCoord'});
-g = graph(s,t,weights,nodeTable,'omitselfloops');
+weights = zeros(1,45); 
+
+
+nodeTable = table(nodes,'VariableNames',{'Name'});
+g = graph(s,t,weights, nodeTable,'omitselfloops');
 plot(g);
 
 
